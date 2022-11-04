1226565d3beb9ea82c7e86ef5209befb97dae100
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-04 14:18:49 -0700

    Add my own database

--- db.js
+++ db.js
@@ -7,8 +7,10 @@ const { Client } = require("pg");
 const DB_URI = process.env.NODE_ENV === "test"
     // ? "postgresql:///lunchly_test"
     // : "postgresql:///lunchly";
-    ? "postgresql://ezray:secret@localhost/lunchly_test"
-    : "postgresql://ezray:secret@localhost/lunchly";
+    // ? "postgresql://ezray:secret@localhost/lunchly_test"
+    // : "postgresql://ezray:secret@localhost/lunchly";
+    ? "postgresql://jcpalca:hello@localhost/lunchly_test"
+    : "postgresql://jcpalca:hello@localhost/lunchly";
 
 let db = new Client({
   connectionString: DB_URI,
